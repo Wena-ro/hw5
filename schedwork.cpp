3b362ecd0bb279a064ffaf945ab5a4c26bb7661f
Rowena Chen <rowenach@usc.edu>
2025-12-04 22:28:03 +0000

    Complete hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<vector<bool>>& isUsed,
+    vector<size_t>& currNum, 
+    size_t day, 
+    size_t slot);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -38,8 +46,78 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    // initialization 
+    size_t ndays = avail.size();
+    size_t kworkers = avail[0].size();
+    sched = DailySchedule(ndays, std::vector<Worker_T> (dailyNeed));
+
+
+    // check avaliability of current worker
+    vector<vector<bool>> isUsed(ndays, vector<bool>(kworkers, false));
+
+
+    // check if the worker meet max maxShifts
+    // if currNum < maxShift, continue
+    vector<size_t> currNum(kworkers, 0);
 
 
 
+
+    // recurrsion function call
+    return scheHelper(avail, dailyNeed, maxShifts, sched, isUsed, currNum, 0, 0);
 }
 
+
+
+bool scheHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<vector<bool>>& isUsed,
+    vector<size_t>& currNum, 
+    size_t day, 
+    size_t slot)
+    {
+
+        // Base case, where everything is fulfilled
+        if(day == avail.size()){
+            return true;
+        }
+
+        size_t nextDay = day;
+        size_t nextSlot = slot + 1;
+
+        if(nextSlot == dailyNeed){
+            nextDay = day + 1;
+            nextSlot = 0;
+        }
+
+        size_t kworkers = avail[0].size();
+
+        for(Worker_T w = 0; w < kworkers; w++){
+            if(avail[day][w] == 1){
+                if(currNum[w] < maxShifts){
+                    if(isUsed[day][w] == false){
+
+
+                        sched[day][slot] = w;
+                        currNum[w]++;
+                        isUsed[day][w] = true;
+
+
+                        if(scheHelper(avail, dailyNeed, maxShifts, sched, isUsed, currNum, nextDay, nextSlot)){
+                            return true;
+                        }
+
+
+                        isUsed[day][w] = false;
+                        currNum[w]--;
+                    }
+                }
+            }
+
+        }
+
+        return false;
+
+    }
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,16 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(
+    const std::string& input,
+    std::string& curr,
+    size_t index,
+    const std::string& fLeft,
+    const std::set<std::string>& dict, 
+    std::set<std::string>& result, 
+    size_t slotLeft);
+
+
 
 
 // Definition of primary wordle function
@@ -23,6 +33,102 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    std::cout << "Input file: " << in << endl << "Floating num: " << floating << endl;
+
+    std::set<std::string> result;
+
+    std::string curr = in;
+
+    size_t slotLeft = 0;
+    for(size_t i = 0; i < in.size(); i++){
+        if(in[i] == '-'){
+            slotLeft++;
+        }
+    }
+
+    wordleHelper(in, curr, 0, floating, dict, result, slotLeft);
+
+    return result;
+
 }
 
 // Define any helper functions here
+
+void wordleHelper(
+    const std::string& input,
+    std::string& curr,
+    size_t index,
+    const std::string& fLeft,
+    const std::set<std::string>& dict, 
+    std::set<std::string>& result, 
+    size_t slotLeft)
+{
+    
+
+    // Cut branch if the rest of slot is not enough for floating
+    if(fLeft.size() > slotLeft){
+      return;
+    }  
+
+    // Base case, if all letters are checked
+    // Check if floating is empty
+    // If empty, check if word is valid
+    // If valid, put it into result and return
+    if(index == input.size()){
+        if(fLeft.empty()){
+            if(dict.count(curr)){
+                result.insert(curr);
+
+            }
+        }
+        return;
+    }
+
+    // Check if current character is '-'
+    // If not, continue into recurrsion
+
+    if(input[index] != '-'){
+        char currChar = input[index];
+        curr[index] = currChar;
+
+        wordleHelper(input, curr, index+1, fLeft, dict, result, slotLeft);
+    }
+
+
+    // If current character is '-'
+    // Check if the rest of slot was only enough to input floating characters
+    // If yes, only use the floating characters for recurrsion
+    else{
+        size_t floatLeft = fLeft.size();
+
+        if(slotLeft == floatLeft){
+            
+            for(size_t i = 0; i < fLeft.size(); i++){
+                char c = fLeft[i];
+                curr[index] = c;
+
+                std::string fNext = fLeft;
+                fNext.erase(i,1);
+
+                wordleHelper(input, curr, index+1, fNext, dict, result, slotLeft-1);
+            }
+            return;
+        }
+    // If slot is more than floating character, implement from a to z
+    // If current input is floating, delete floating
+    // Pass rest to recurrsion
+
+        for(char c = 'a'; c <= 'z'; c++){
+            curr[index] = c;
+
+            std::string fNext = fLeft;
+
+            size_t pos = fNext.find(c);
+            if(pos != std::string::npos){
+                fNext.erase(pos,1);
+            }
+
+            wordleHelper(input, curr, index+1, fNext, dict, result, slotLeft-1);
+        }
+    }
+}
